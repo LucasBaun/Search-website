0fb23cedeaf2e8ef838b0429df4b8aa9cbc9b256
kalleland <anonymistan@hotmail.se>
2018-05-02 22:34:33 +0200

    firs commit
    added welcome text, search box and background image

---
+++ script.js
@@ -0,0 +1,36 @@
+var questions = [
+  {question:"Search on google?"}
+]
+
+;(function(){
+
+  var position = 0
+
+  putQuestion()
+
+  progressButton.addEventListener('click', validate)
+  inputField.addEventListener('keyup', function(e){
+    transform(0, 0)
+    if(e.keyCode == 13) validate()
+  })
+
+  function putQuestion() {
+    inputLabel.innerHTML = questions[position].question
+    inputField.value = ''
+    inputField.type = questions[position].type || 'text'
+    inputField.focus()
+    showCurrent()
+  }
+
+  function showCurrent(callback) {
+    inputContainer.style.opacity = 1
+    inputProgress.style.transition = ''
+    inputProgress.style.width = '100%'
+    setTimeout(callback, wTime)
+  }
+
+  function transform(x, y) {
+    register.style.transform = 'translate(' + x + 'px ,  ' + y + 'px)'
+  }
+
+}())
